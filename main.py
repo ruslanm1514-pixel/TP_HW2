0adb96053185cf5a602c7bbc682ccbdce70f1d91
Фамилия Имя <you@edu.hse.ru>
2026-06-03 20:28:10 +0300

    Добавил класс Recipe

--- main.py
+++ main.py
@@ -24,3 +24,37 @@ class Ingredient:
         if not isinstance(other, Ingredient):
             return False
         return self.name == other.name and self.unit == other.unit
+
+class Recipe:
+    def __init__(self, title, ingredients):
+        self.title = title
+        self.ingredients = ingredients
+
+    def add_ingredient(self, ingredient):
+        for i in self.ingredients:
+            if i == ingredient:
+                i.quantity += ingredient.quantity
+                return
+        self.ingredients.append(ingredient)
+
+    @staticmethod
+    def is_valid_ratio(ratio):
+        try:
+            return float(ratio) > 0
+        except (TypeError, ValueError):
+            return False
+
+    def scale(self, ratio):
+        if not self.is_valid_ratio(ratio):
+            raise ValueError
+        
+        return Recipe(self.title, [Ingredient(i.name, i.quantity * ratio, i.unit) for i in self.ingredients])
+
+    def __len__(self):
+        return len(self.ingredients)
+
+    def __str__(self):
+        result = f"{self.title}:\n"
+        for ing in self.ingredients:
+            result += f"  - {ing}\n"
+        return result.rstrip()
